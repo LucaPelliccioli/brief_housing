db2bc9ee1232f380e4839905e3642b077d8a9227
Luca <luca.pelliccioli.pro@gmail.com>
2021-06-25 10:36:08 +0200

    essai

--- Sprint2/housing_flask/housing/app/views.py
+++ Sprint2/housing_flask/housing/app/views.py
@@ -1,5 +1,4 @@
 # coding: utf8
-from app import app
 from flask import render_template, request, abort, redirect, url_for
 import datetime
 import pandas as pd
@@ -12,39 +11,36 @@ from app import models
 # A chaque fois qu'on ouvrira un navigateur pour accéder à l'indexe, c'est cette fonction qui sera appelé
 # @app.route est un décorateur de la varibale app qui va encapsuler la fonction index() et acheminer les demande vers cette fonction
 
-@app.route('/')
-def index():
-    date = datetime.datetime.now().strftime("%x %X")
-    return render_template( 'index.html', date=date)
+def init_app(app):
+    @app.route('/')
+    def index():
+        date = datetime.datetime.now().strftime("%x %X")
+        return render_template( 'index.html', date=date)
 
-@app.route('/dashboard')
-def dashboard():
-    models.graphique()
-    date = datetime.datetime.now().strftime("%x %X")
-    return render_template( 'dashboard.html', date=date)
+    @app.route('/dashboard')
+    def dashboard():
+        models.graphique()
+        date = datetime.datetime.now().strftime("%x %X")
+        return render_template( 'dashboard.html', date=date)
 
-@app.route('/formulaire_predict')
-def formulaire_predict():
-    date = datetime.datetime.now().strftime("%x %X")
-    return render_template( 'formulaire_predict.html', date=date)
+    @app.route('/formulaire_predict')
+    def formulaire_predict():
+        date = datetime.datetime.now().strftime("%x %X")
+        return render_template( 'formulaire_predict.html', date=date)
 
-@app.route('/predict', methods = ['POST', 'GET'])
-def predict():
-    nop = request.form['ocean']
-    mi = request.form['revenu']
-    predict = models.predict(mi, nop)
-    date = datetime.datetime.now().strftime("%x %X")
-    return render_template( 'predict.html', date=date, nop=nop, mi=mi, predict=predict)
+    @app.route('/predict', methods = ['POST', 'GET'])
+    def predict():
+        nop = request.form['ocean']
+        mi = request.form['revenu']
+        predict = models.predict(mi, nop)
+        date = datetime.datetime.now().strftime("%x %X")
+        return render_template( 'predict.html', date=date, nop=nop, mi=mi, predict=predict)
 
-@app.route('/ajout_bien', methods = ['POST', 'GET'])
-def ajout_bien():
-    return render_template("Pages/formulaire_predict.html")
-    
-@app.route('/modifier_bien', methods = ['POST', 'GET'])
-def modifier_bien():
-    data = models.sqldata()
-<<<<<<< Updated upstream
-    return render_template("Pages/modifier_bien.html", data)
-=======
-    return render_template("Pages/modifier_bien.html", d=data)
->>>>>>> Stashed changes
+    @app.route('/ajout_bien', methods = ['POST', 'GET'])
+    def ajout_bien():
+        return render_template("Pages/formulaire_predict.html")
+        
+    @app.route('/modifier_bien', methods = ['POST', 'GET'])
+    def modifier_bien():
+        data = models.sqldata()
+        return render_template("Pages/modifier_bien.html", d=data)
